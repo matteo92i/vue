325990edd50f131cf5492dff8a47ca7ba2f1696d
Matteo92i <matteo.iovane@gmail.com>
2021-09-30 15:03:31 +0200

    inizio

---
+++ src/components/Listadischi.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="box-container"></div>
+        </div>
+
+    </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+    data
+
+
+    mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(risposta)
+    console.log(risposta)
+    },
+}
+</script>
+
+
+<style>
+
+</style>
+   
+
+    
+      
+      
+
+      
+
+  
+
+      
+
+
+
+
+
+
